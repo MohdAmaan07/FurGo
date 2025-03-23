50511bdfee71838b2d59452d61a0fd299779e18d
Mohd Amaan <mohd.amaan8642@gmail.com>
2025-03-23 16:08:41 +0530

    Add post

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Posts
+from django.contrib.auth import get_user_model
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username')
 
 class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
     class Meta:
         model = Posts
-        fields = ('id', 'title', 'author', 'content', 'created_at', 'updated_at')
+        fields = ('id', 'author','title', 'content', 'posts_image','created_at', 'updated_at')
