26af9cf24aac82b4e8505bda0bae4dc8f03054dc
Mohd Amaan <mohd.amaan8642@gmail.com>
2025-03-26 18:20:42 +0530

    Connect Store API

--- backend/FurGo/urls.py
+++ backend/FurGo/urls.py
@@ -19,13 +19,19 @@ from django.urls import path
 from django.urls.conf import include
 from django.conf.urls.static import static
 from django.conf import settings
+import debug_toolbar
+
+admin.site.site_header = 'Storefront Admin'
+admin.site.index_title = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('store/', include('store.urls')),
     path('pets/', include('pets.urls')),
     path('posts/', include('posts.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]   
 
 if settings.DEBUG:
